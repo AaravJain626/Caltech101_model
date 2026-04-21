e6a42fdcfb95d8d23be30f6d20806802dbc6c2b7
Aarav Jain <ajvid220512@gmail.com>
2026-04-21 20:37:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,3 +10,19 @@ import numpy as np
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
+data = torchvision.datasets.Caltech101(
+    root = "data",
+    download = True,
+    transform = ToTensor(),
+    target_transform = None
+)
+     
+
+# train test split
+train_data = data[int(len(data) * 0.8)]
+len(x)
+     
+2
+
+len(train_data)
+     
